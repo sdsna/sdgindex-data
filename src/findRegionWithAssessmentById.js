0232fb8c0c7fc84bc725d66216ce7839098773f4
Sakala85 <melchiorbengtsson@gmail.com>
2021-04-30 16:18:31 +0200

    double check fixes

--- src/findRegionWithAssessmentById.js
+++ src/findRegionWithAssessmentById.js
@@ -4,7 +4,6 @@ import { findObservationByRegionAndAssessment } from "./findObservationByRegionA
  * get specific region by Id with a specific assessment
  * @param {store} dataStore - The store where the data are loaded
  * @param {Id} regionId - The specific region ID
- * @param {Object} goal - The specific goal
  * @return {Object} return an object with the Region Object and the assessment Object {{id:..., name:..., s:..., ...}, {id:..., ...}}
  */
 export const findRegionWithAssessmentById = (
