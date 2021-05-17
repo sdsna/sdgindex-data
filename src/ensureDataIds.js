f317985be805ab67daea7dfd838c51213d979203
Finn Woelm <finn.woelm@unsdsn.org>
2021-05-17 15:15:59 +0200

    Documentation: Fix ESDoc errors
    Some documentations did not match the function signature.

--- src/ensureDataIds.js
+++ src/ensureDataIds.js
@@ -2,9 +2,10 @@
  * Ensure dataId prop is present on the object, otherwise load dataId from store
  * The dataId prop is added permanently to the object, so that we don't have to
  * reload it again and again.
- * @param {Object} dataStore - The store where the data are loaded
- * @param {Object} region - The region object {id:..., name:..., ...}
- * @param {Object} assessment - The assessment object {id:..., name:..., ...}
+ * @param {{Object}} param
+ * @param {{Object}} param.dataStore - The store where the data are loaded
+ * @param {Object} param.region - The region object {id:..., name:..., ...}
+ * @param {Object} param.assessment - The assessment object {id:..., name:..., ...}
  */
 export const ensureDataIds = ({ dataStore, region, assessment }) => {
   if (region && !region.hasOwnProperty("dataId"))
--- src/findRegionWithAssessmentById.js
+++ src/findRegionWithAssessmentById.js
@@ -3,6 +3,7 @@ import { findObservationByRegionAndAssessment } from "./findObservationByRegionA
 /**
  * get specific region by Id with a specific assessment
  * @param {Object} dataStore - The store where the data are loaded
+ * @param {Object} assessment - the assessment to get
  * @param {string} regionId - The specific region ID
  * @return {Object} return an object with the Region Object and the assessment Object {{id:..., name:..., s:..., ...}, {id:..., ...}}
  */
