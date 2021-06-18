9ab98951e7ebe4dc7414af9c9242927db3990df7
Finn Woelm <finn.woelm@unsdsn.org>
2021-06-18 14:16:55 +0200

    Fix getYearAsText: Return fallback string when year is null
    Return fallback string when year is null. Previously, only returned
    fallback string if value was null. But this could cause an application
    crash if year was null, but value was not.

--- src/observations/getYearAsText.js
+++ src/observations/getYearAsText.js
@@ -13,9 +13,7 @@ export const getYearAsText = (observation, fallback = "Year unavailable") => {
   const year = getYear(observation);
   const value = getValue(observation);
 
-  // TODO: Do we really want to check if value != null rather than year != null
-  // We should double check this!
-  if (value != null) return year.toFixed(0);
+  if (value != null && year != null) return year.toFixed(0);
 
   return fallback;
 };
