3a10be18b4be72879fd4193af75b2cf4c4541cd5
Finn Woelm <finn.woelm@unsdsn.org>
2021-07-08 16:59:50 +0200

    Refactor: Define property keys for observations
    Define property keys (SCORE_KEY, VALUE_KEY, etc...) that define the 
    conversion from long name to short name.

--- src/observations/isImputed.js
+++ src/observations/isImputed.js
@@ -1,7 +1,9 @@
+import { IS_IMPUTED_KEY } from "./config";
+
 /**
  * Check if the given observation is imputed.
  * @param {Object} observation
  * @return {bool} Return true if observation is imputed
  */
 export const isImputed = (observation) =>
-  Object.prototype.hasOwnProperty.call(observation, "i");
+  Object.prototype.hasOwnProperty.call(observation, IS_IMPUTED_KEY);
