61ddad0105e30a9842df2f1c17eae01a61b53ef5
Finn Woelm <finn.woelm@unsdsn.org>
2021-07-08 13:20:26 +0200

    Improve docs: parse/addIndicator

--- src/parse/addIndicator.js
+++ src/parse/addIndicator.js
@@ -6,14 +6,42 @@ import { getIndicators } from "../getIndicators";
 /**
  * Add indicator to the dataStore
  * @param {Object} dataStore - The store where the data are saved
+ * @param {Object} indicator
+ * @param {string} indicator.id - The unique ID for the indicator
+ * @param {number} indicator.goalNumber -
+ * The goal to which the indicator belongs, e.g., `1` for SDG 1, `2` for SDG 2.
+ * @param {string} [indicator.labelWithUnit] -
+ * The label for the indicater, including the unit. Used to automatically
+ * generate the slug, label, and unit. Alternatively, slug, label, and unit
+ * properties can be set manually.
+ * @param {string} [indicator.slug] -
+ * The slug for the indicator, usually automatically generated from
+ * labelWithUnit.
+ * @param {string} [indicator.label] -
+ * The label for the indicator, usually automatically generated from
+ * labelWithUnit.
+ * @param {string} [indicator.unit] -
+ * The unit for the indicator, usually automatically generated from
+ * labelWithUnit.
+ * @param {...Object} [indicator.params] -
+ * any other params to add to the indicator, such as description or source
  */
-export const addIndicator = (dataStore, { labelWithUnit, ...params }) => {
-  const label = getLabelWithoutUnit(labelWithUnit);
+export const addIndicator = (
+  dataStore,
+  { id, goalNumber, labelWithUnit, slug, label, unit, ...params }
+) => {
+  if (!label) label = getLabelWithoutUnit(labelWithUnit);
+  if (!unit) unit = getIndicatorUnit(labelWithUnit);
+  if (!slug) slug = urlSlug(label);
+
+  if (!dataStore.assessments) dataStore.assessments = [];
   dataStore.assessments.push({
-    slug: urlSlug(label),
-    label,
-    unit: getIndicatorUnit(labelWithUnit),
+    id,
     dataId: getIndicators(dataStore).length + 1,
+    goalNumber,
+    slug,
+    label,
+    unit,
     ...params,
     type: "indicator",
   });
--- tests/parse/addIndicator.test.js
+++ tests/parse/addIndicator.test.js
@@ -1,15 +1,20 @@
 import { addIndicator } from "@sdgindex/data/parse";
-import { buildIndicator } from "testHelpers/builders";
 
-it("add indicator to the dataStore", () => {
-  const dataStore = { assessments: [] };
-  const labelWithUnit = "Poverty headcount ratio at $1.90/day (%)";
+let dataStore = {};
+beforeEach(() => {
+  // Clear dataStore
+  dataStore = {};
+});
+
+it("adds the indicator to the dataStore", () => {
   addIndicator(dataStore, {
-    labelWithUnit,
     id: "sdg1_wpc",
     goalNumber: 1,
+    labelWithUnit: "Poverty headcount ratio at $1.90/day (%)",
   });
-  const indicator = buildIndicator({
+
+  expect(dataStore.assessments).toHaveLength(1);
+  expect(dataStore.assessments[0]).toMatchObject({
     id: "sdg1_wpc",
     dataId: 1,
     goalNumber: 1,
@@ -18,7 +23,32 @@ it("add indicator to the dataStore", () => {
     unit: "%",
     type: "indicator",
   });
+});
 
-  delete indicator.description;
-  expect(dataStore.assessments[0]).toEqual(indicator);
+describe("when manually providing the slug", () => {
+  it("it uses the user-provided slug", () => {
+    addIndicator(dataStore, {
+      id: "sdg1_wpc",
+      goalNumber: 1,
+      labelWithUnit: "Poverty headcount ratio at $1.90/day (%)",
+      slug: "my-custom-slug",
+    });
+
+    expect(dataStore.assessments[0]).toHaveProperty("slug", "my-custom-slug");
+  });
+});
+
+describe("when adding 10 indicators", () => {
+  it("it sets a unique dataId for each one", () => {
+    for (let i = 0; i < 10; i++) {
+      addIndicator(dataStore, {
+        id: "sdg1_wpc",
+        goalNumber: 1,
+        labelWithUnit: "Poverty headcount ratio at $1.90/day (%)",
+      });
+    }
+
+    const dataIds = dataStore.assessments.map((indicator) => indicator.dataId);
+    expect(Array.from(new Set(dataIds))).toHaveLength(10);
+  });
 });
