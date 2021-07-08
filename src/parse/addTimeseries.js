0fa8bfa8c0efe51c63f3e43ee81562c9cc7af0ba
Finn Woelm <finn.woelm@unsdsn.org>
2021-07-08 18:21:17 +0200

    Improve docs: parse/addTimeseries

--- src/parse/addTimeseries.js
+++ src/parse/addTimeseries.js
@@ -1,19 +1,38 @@
 import { END_YEAR, START_YEAR } from "../timeseries/config";
+import { roundNumber } from "./roundNumber";
 
 /**
- * Add timeseries to the dataStore
+ * Add timeseries to the dataStore. If the values of all data points are null,
+ * the series is not added.
  * @param {Object} dataStore - The store where the data are saved
+ * @param {Object} timeseries
+ * @param {Object} timeseries.region -
+ * the region to which the timeseries belongs
+ * @param {Object} timeseries.assessment -
+ * the assessment to which the timeseries belongs
+ * @param {Object[]} timeseries.dataPoints - an array of data points
+ * @param {number} timeseries.dataPoints[].year - the year of the data point
+ * @param {number} timeseries.dataPoints[].value - the value of the data point
  */
-export const addTimeseries = (dataStore, { region, assessment, values }) => {
+export const addTimeseries = (
+  dataStore,
+  { region, assessment, dataPoints }
+) => {
+  // Convert dataPoints into a values array
+  const values = [];
+  for (let year = START_YEAR; year <= END_YEAR; year++) {
+    const dataPoint = dataPoints.find((point) => point.year === year);
+    const value = dataPoint ? roundNumber(dataPoint.value, 2) : null;
+    values.push(value);
+  }
+
+  // If the values of all data points are null, do not add the timeseries to the
+  // store.
+  if (values.every((value) => value == null)) return;
+
+  if (!dataStore.timeseries) dataStore.timeseries = [];
   dataStore.timeseries.push({
     id: `${region.dataId}-${assessment.dataId}`,
-    v: Array.from({ length: END_YEAR - START_YEAR + 1 }).map((_, index) => {
-      const value = values.find((row) => row.year === START_YEAR + index).value;
-      if (value == null) {
-        return null;
-      } else {
-        return value;
-      }
-    }),
+    v: values,
   });
 };
--- tests/parse/addTimeseries.test.js
+++ tests/parse/addTimeseries.test.js
@@ -1,46 +1,96 @@
 import { addTimeseries } from "@sdgindex/data/parse";
+import { buildIndicator, buildRegion } from "testHelpers/builders";
+
+let dataStore = {};
+beforeEach(() => {
+  // Clear dataStore
+  dataStore = {};
+});
 
 it("add timeseries to the dataStore", () => {
-  const dataStore = { timeseries: [] };
-  const region = { dataId: 1 };
-  const assessment = { dataId: "TOT" };
-  const values = [
-    { year: 2000, value: 83.45 },
-    { year: 2001, value: 83.37 },
-    { year: 2002, value: 83.6 },
-    { year: 2003, value: 83.28 },
-    { year: 2004, value: 83.39 },
-    { year: 2005, value: 84 },
-    { year: 2006, value: 83.5 },
-    { year: 2007, value: 83.57 },
-    { year: 2008, value: 83.89 },
-    { year: 2009, value: 85.36 },
-    { year: 2010, value: 85.3 },
-    { year: 2011, value: 85.55 },
-    { year: 2012, value: 85.9 },
-    { year: 2013, value: 85.91 },
-    { year: 2014, value: 86.04 },
-    { year: 2015, value: 85.92 },
-    { year: 2016, value: 85.79 },
-    { year: 2017, value: 85.85 },
-    { year: 2018, value: 85.53 },
-    { year: 2019, value: 85.79 },
-    { year: 2020, value: 85.89 },
-    { year: 2021, value: 85.89 },
-  ];
-
   addTimeseries(dataStore, {
-    region,
-    assessment,
-    values,
+    region: buildRegion({ dataId: 1 }),
+    assessment: buildIndicator({ dataId: "TOT" }),
+    dataPoints: [
+      { year: 2000, value: 1 },
+      { year: 2001, value: 2 },
+      { year: 2002, value: 3 },
+      { year: 2003, value: 4 },
+      { year: 2004, value: 5 },
+      { year: 2005, value: 6 },
+      { year: 2006, value: 7 },
+      { year: 2007, value: 8 },
+      { year: 2008, value: 9 },
+      { year: 2009, value: 10 },
+      { year: 2010, value: 9 },
+      { year: 2011, value: 8 },
+      { year: 2012, value: 7 },
+      { year: 2013, value: 6 },
+      { year: 2014, value: 5 },
+      { year: 2015, value: 4 },
+      { year: 2016, value: 3 },
+      { year: 2017, value: 2 },
+      { year: 2018, value: 1 },
+      { year: 2019, value: 0 },
+      { year: 2020, value: -1 },
+      { year: 2021, value: -2 },
+    ],
   });
 
   expect(dataStore.timeseries[0]).toEqual({
     id: "1-TOT",
+    v: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2],
+  });
+});
+
+it("rounds values to two decimals", () => {
+  addTimeseries(dataStore, {
+    region: buildRegion({ dataId: 1 }),
+    assessment: buildIndicator({ dataId: "TOT" }),
+    dataPoints: [{ year: 2004, value: 115.2365 }],
+  });
+
+  expect(dataStore.timeseries[0]).toMatchObject({
     v: [
-      83.45, 83.37, 83.6, 83.28, 83.39, 84, 83.5, 83.57, 83.89, 85.36, 85.3,
-      85.55, 85.9, 85.91, 86.04, 85.92, 85.79, 85.85, 85.53, 85.79, 85.89,
-      85.89,
+      null,
+      null,
+      null,
+      null,
+      115.24,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
     ],
+  });
+});
+
+describe("when adding timeseries without data points", () => {
+  it("does not add timeseries", () => {
+    addTimeseries(dataStore, {
+      region: buildRegion(),
+      assessment: buildIndicator(),
+      dataPoints: [
+        {
+          year: 2005,
+          value: null,
+        },
+      ],
+    });
+
+    expect(dataStore).not.toHaveProperty("timeseries");
   });
 });
