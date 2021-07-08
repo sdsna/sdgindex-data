e52c42ccc060be67c2828d91b07c3fbda1ff6e46
Finn Woelm <finn.woelm@unsdsn.org>
2021-07-08 17:02:48 +0200

    Improve docs: parse/addObservation

--- src/parse/addObservation.js
+++ src/parse/addObservation.js
@@ -1,29 +1,105 @@
+import {
+  SCORE_KEY,
+  RANK_KEY,
+  RATING_KEY,
+  TREND_KEY,
+  VALUE_KEY,
+  YEAR_KEY,
+  IS_IMPUTED_KEY,
+} from "../observations/config";
+import { roundNumber } from "./roundNumber";
+
+// Property names are shortened to a single letter to save space
 const PROPERTIES = {
-  score: "s",
-  rank: "r",
-  color: "c",
-  value: "v",
-  arrow: "a",
-  year: "y",
-  impute: "i",
+  score: {
+    name: SCORE_KEY,
+    formatter: (score) => roundNumber(score, 2),
+  },
+  rank: {
+    name: RANK_KEY,
+    formatter: (rank) => parseInt(rank),
+  },
+  rating: {
+    name: RATING_KEY,
+    formatter: (rating) => {
+      if (rating == null || rating === "grey" || rating === "") return "gray";
+      return rating;
+    },
+  },
+  trend: {
+    name: TREND_KEY,
+    formatter: (trend) => {
+      if (trend == null || trend === "") return null;
+      return trend;
+    },
+  },
+  value: {
+    name: VALUE_KEY,
+    formatter: (value) => roundNumber(value, 2),
+  },
+  year: {
+    name: YEAR_KEY,
+  },
+  isImputed: {
+    name: IS_IMPUTED_KEY,
+    formatter: (isImputed) => (isImputed ? 1 : 0),
+  },
 };
 
 /**
  * Add observation to the dataStore
  * @param {Object} dataStore - The store where the data are saved
+ * @param {Object} observation
+ * @param {Object} observation.region -
+ * the region to which the observation belongs
+ * @param {Object} observation.assessment -
+ * the assessment to which the observation belongs
+ * @param {number} [observation.score] -
+ * the score of the region on the assessment
+ * @param {number} [observation.rank] -
+ * the rank of the region on the assessment
+ * @param {string} [observation.rating] -
+ * the rating of the region on the assessment,
+ * e.g., `green`, `yellow`, `orange`, `red`, or `gray`
+ * @param {?string} [observation.trend] -
+ * the trend of the region on the assessment,
+ * e.g., `↑`, `➚`, `→`, `↓`, or `null`
+ * @param {number} [observation.value] -
+ * the raw value of the region on the assessment (applicable to indicators only)
+ * @param {number} [observation.year] -
+ * the raw value of the region on the assessment (applicable to indicators only)
+ * @param {boolean} [observation.isImputed=false] -
+ * whether the observation is imputed (`true` or `false`)
+ * @param {...Object} [observation.params] -
+ * any other properties to store on the observation
  */
 export const addObservation = (
   dataStore,
-  { region, assessment, ...params }
+  { region, assessment, ...observation }
 ) => {
-  Object.entries(PROPERTIES).map(([longName, shortName]) => {
-    if (Object.prototype.hasOwnProperty.call(params, longName)) {
-      params[shortName] = params[longName];
-      delete params[longName];
+  // Shorten property names and format values
+  Object.keys(PROPERTIES).map((propertyName) => {
+    if (Object.prototype.hasOwnProperty.call(observation, propertyName)) {
+      const { name: shortName, formatter } = PROPERTIES[propertyName];
+
+      // Format value using property formatter
+      if (formatter != null)
+        observation[propertyName] = formatter(observation[propertyName]);
+
+      // Shorten property name
+      observation[shortName] = observation[propertyName];
+      delete observation[propertyName];
     }
   });
+
+  // Remove isImputed flag, unless observation is imputed
+  const isImputedKey = PROPERTIES.isImputed.name;
+  if (observation[isImputedKey] !== 1) delete observation[isImputedKey];
+
+  // Push observation
+  if (!dataStore.observations) dataStore.observations = [];
   dataStore.observations.push({
     id: `${region.dataId}-${assessment.dataId}`,
-    ...params,
+    ...observation,
   });
 };
--- tests/parse/addObservation.test.js
+++ tests/parse/addObservation.test.js
@@ -1,23 +1,54 @@
 import { addObservation } from "@sdgindex/data/parse";
+import { buildIndicator, buildRegion } from "testHelpers/builders";
 
-it("add observation to the dataStore", () => {
-  const dataStore = { observations: [] };
-  const region = { dataId: 1 };
-  const assessment = { dataId: 4 };
+let dataStore = {};
+beforeEach(() => {
+  // Clear dataStore
+  dataStore = {};
+});
+
+it("adds the observation to the dataStore", () => {
+  const region = buildRegion({ dataId: 1 });
+  const indicator = buildIndicator({ dataId: 4 });
+
   addObservation(dataStore, {
     region,
-    assessment,
-    value: 2.5,
-    color: "green",
-    arrow: "↑",
+    assessment: indicator,
+    value: 2.575241,
+    rating: "green",
+    trend: "↑",
     year: 2018,
   });
-  console.log(dataStore.observations);
+
   expect(dataStore.observations[0]).toEqual({
     id: "1-4",
-    v: 2.5,
+    v: 2.58,
     c: "green",
     a: "↑",
     y: 2018,
+  });
+});
+
+describe("when passing isImputed = true", () => {
+  it("adds imputation flag", () => {
+    addObservation(dataStore, {
+      region: buildRegion(),
+      assessment: buildIndicator(),
+      isImputed: true,
+    });
+
+    expect(dataStore.observations[0]).toHaveProperty("i", 1);
+  });
+});
+
+describe("when passing isImputed = false", () => {
+  it("does not add imputation flag", () => {
+    addObservation(dataStore, {
+      region: buildRegion(),
+      assessment: buildIndicator(),
+      isImputed: false,
+    });
+
+    expect(dataStore.observations[0]).not.toHaveProperty("i");
   });
 });
