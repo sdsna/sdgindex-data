3be1ddd4b9386dee307bf92c8f17ccb8739ec54a
Finn Woelm <finn.woelm@unsdsn.org>
2021-08-19 16:28:08 +0200

    addOverallAssessment: Remove mention of "country" from description
    Some indices are focused on countries, some on states, some on cities.
    Therefore, it's best to keep descriptions generic and not specific to a
    type of region.

--- src/parse/addOverallAssessment.js
+++ src/parse/addOverallAssessment.js
@@ -15,7 +15,7 @@ export const addOverallAssessment = (params = {}) =>
     slug: "overall",
     label: "Overall score",
     description:
-      "The overall score measures a country's total progress towards achieving all 17 SDGs. The score can be interpreted as a percentage of SDG achievement. A score of 100 indicates that all SDGs have been achieved.",
+      "The overall score measures the total progress towards achieving all 17 SDGs. The score can be interpreted as a percentage of SDG achievement. A score of 100 indicates that all SDGs have been achieved.",
     type: "custom",
     ...params,
   });
